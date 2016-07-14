f4d0ccdc1c548264cd4bedd1d7873058a35eec59
Orel <xvab2byte@gmail.com>
2016-07-14 22:54:43 +0300

    Apanel notebook

--- public/js/common.js
+++ public/js/common.js
@@ -5,5 +5,25 @@ $(function () {
 function notebook(formSelector, secret) {
     document.querySelector(formSelector).querySelector('button').onclick = function (e) {
         e.preventDefault();
+
+        var btnElement = this;
+
+        btnElement.setAttribute('disabled', true);
+
+        $.ajax({
+            url: this.parentNode.getAttribute('action'),
+            method: 'POST',
+            data: {
+                content: GibberishAES.enc(this.parentNode.querySelector('textarea').value, key)
+            },
+            success: function (response) {
+                btnElement.removeAttribute('disabled');
+            },
+            error: function (err) {
+                btnElement.setAttribute('class', 'btn btn-danger');
+
+                console.log(err.responseText);
+            }
+        });
     };
 }
