1a3fb874621c0b5c2a0785288fd2d79186cf1fbd
orel2888 <xvab2byte@gmail.com>
2016-08-05 15:59:27 +0300

    Remove encrypte

--- app/Goods.php
+++ app/Goods.php
@@ -3,22 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Ghost\Libs\GibberishAES;
 
 class Goods extends Model
 {
     public $table = 'goods';
     public $guarded = ['id'];
-
-    public function getNameAttribute($value)
-    {
-        if (base64_encode(base64_decode($value)) === $value) {
-            return GibberishAES::dec($value, env('K5'));
-        } else {
-            return $value;
-        }
-    }
-
+    
     public function goodsPrice()
     {
         return $this->hasMany('App\GoodsPrice');
--- app/GoodsPrice.php
+++ app/GoodsPrice.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Ghost\Libs\GibberishAES;
 
 class GoodsPrice extends Model
 {
@@ -13,14 +12,5 @@ class GoodsPrice extends Model
     public function goods()
     {
         return $this->belongsTo('App\Goods', 'goods_id', 'id');
-    }
-
-    public function getAddressAttribute($value)
-    {
-        if (base64_encode(base64_decode($value)) === $value) {
-            return GibberishAES::dec($value, env('K5'));
-        } else {
-            return $value;
-        }
     }
 }
