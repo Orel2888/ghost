8664e40b8773687c708f4fb9aaa78f0c9c6ab209
orel2888 <xvab2byte@gmail.com>
2016-08-09 00:31:25 +0300

    Apanel repository protected method

--- app/Ghost/Apanel/Controllers/ApanelBaseController.php
+++ app/Ghost/Apanel/Controllers/ApanelBaseController.php
@@ -5,6 +5,7 @@ namespace App\Ghost\Apanel\Controllers;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use App\Ghost\Repositories\Goods\GoodsManager;
+use App\Ghost\Apanel\Repository\ApanelRepository;
 
 class ApanelBaseController extends BaseController
 {
@@ -20,9 +21,15 @@ class ApanelBaseController extends BaseController
      */
     protected $goodsManager;
 
+    /**
+     * @var ApanelRepository
+     */
+    protected $apanelRepo;
+
     public function __construct()
     {
         $this->request      = app('request');
         $this->goodsManager = new GoodsManager();
+        $this->apanelRepo   = new ApanelRepository();
     }
 }
