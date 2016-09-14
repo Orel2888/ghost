d86b6af58b5aeaeae74f358df0f9e81140f0b5dc
Orel <sql@list.ru>
2016-09-14 18:32:16 +0300

    Routes miner

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -72,11 +72,13 @@ Route::group(['middleware' => ['web'], 'namespace' => 'App\Ghost\Apanel\Controll
 
         /**
          * Soldiers
+         * Resource miner
          */
-        
-        Route::get('miner', 'ApanelMinerController@getIndex');
-        Route::get('miner/info/{id}', 'ApanelMinerController@getInfo')->where('id', '\w+');
-
+        Route::get('miner/{miner_id}/payment_create', ['uses' => 'ApanelMinerController@payment_create', 'as' => 'apanel.miner.payment_create']);
+        Route::get('miner/{miner_id}/payment_store', ['uses' => 'ApanelMinerController@payment_store', 'as' => 'apanel.miner.payment_store']);
+        Route::get('miner/{miner_id}/delete-confirm', ['uses' => 'ApanelMinerController@deleteConfirm', 'as' => 'apanel.miner.delete_confirm']);
+        Route::get('miner/{miner_id}/delete', ['uses' => 'ApanelMinerController@delete', 'as' => 'apanel.miner.delete']);
+        Route::resource('miner', 'ApanelMinerController');
     });
 
     Route::get('login', 'ApanelAuthController@getLogin');
