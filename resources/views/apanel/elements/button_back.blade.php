99b2806af390f693b8f0d72be1d49a3d22359485
Orel <xvab2byte@gmail.com>
2016-09-18 16:00:21 +0300

    Other

---
+++ resources/views/apanel/elements/button_back.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: orel
+ * Date: 18.09.16
+ * Time: 14:51
+ */
---
+++ resources/views/apanel/miner/payment/index.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: orel
+ * Date: 18.09.16
+ * Time: 15:43
+ */
