c904f4fe025a91a832080817160e2bb0dca7cdc2
Orel <xvab2byte@gmail.com>
2016-09-26 21:14:53 +0300

    Payments listing

--- resources/views/apanel/miner/payment/index.blade.php
+++ resources/views/apanel/miner/payment/index.blade.php
@@ -21,8 +21,47 @@
                 </div>
 
                 <table class="table table-bordered table-hover">
-
+                    <thead>
+                    <th>ID</th>
+                    <th>Минер ID</th>
+                    <th>Сумма</th>
+                    <th>Кол-во находов</th>
+                    <th>Кол-во ненаходов</th>
+                    <th>Статус</th>
+                    <th>Дата</th>
+                    <th>Действие</th>
+                    </thead>
+                    <tbody>
+                    @forelse ($payments as $payment)
+                        <tr>
+                            <td>{{ $payment->id }}</td>
+                            <td>{{ $payment->miner_id }}</td>
+                            <td>{{ $payment->amount }}</td>
+                            <td>{{ $payment->counter_goods_ok }}</td>
+                            <td>{{ $payment->counter_goods_fail }}</td>
+                            <td>{{ $payment->status }}</td>
+                            <td>{{ $payment->created_at->format('d-m-Y H:i:s') }}</td>
+                            <td>
+                                <div class="btn-group btn-group-xs">
+                                    <a href="{{ '' }}" class="btn btn-success btn-xs">Выплатить</a>
+                                    <a href="{{ '' }}" class="btn btn-danger btn-xs">Отклонить</a>
+                                    <a href="{{ '' }}" class="btn btn-info btn-xs">В обработке</a>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="8">Нет данных</td>
+                        </tr>
+                    @endforelse
+                    </tbody>
                 </table>
+
+                @if (($paginate = $payments->links()) != '')
+                    <div class="panel-footer">
+                        {{ $paginate }}
+                    </div>
+                @endif
             </div>
         </div>
 
