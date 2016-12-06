652fdfd41241eb369809f215b9918ca5a4308211
Orel <xvab2byte@gmail.com>
2016-12-06 23:17:48 +0300

    Add relations

--- app/GoodsPurchase.php
+++ app/GoodsPurchase.php
@@ -9,4 +9,13 @@ class GoodsPurchase extends Model
     public $table = 'goods_purchases';
     public $guarded = ['id'];
 
+    public function client()
+    {
+        return $this->hasOne('App\Client', 'id', 'client_id');
+    }
+
+    public function goods()
+    {
+        return $this->hasOne('App\Goods', 'id', 'goods_id');
+    }
 }
