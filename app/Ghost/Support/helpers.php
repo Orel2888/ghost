6a3ed00cac43c4eea664bdfa1dceae035a03ec5d
Orel <xvab2byte@gmail.com>
2016-12-08 00:02:19 +0300

    Changes wcorrect

--- app/Ghost/Support/helpers.php
+++ app/Ghost/Support/helpers.php
@@ -3,14 +3,6 @@
 if (!function_exists('wcorrect')) {
     function wcorrect($weight) {
 
-        if (substr($weight, -1) == 0) {
-            $weight = substr($weight, 0, -1);
-        }
-
-        if (substr($weight, 0, 1) != 0) {
-            $weight = substr($weight, 0, -2);
-        }
-
-        return (string)$weight;
+        return preg_replace('|\.?0+$|', '', $weight);
     }
 }
