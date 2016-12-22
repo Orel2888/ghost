a0f04067a04470ea870010ae8f55e375d5ca32df
Orel <xvab2byte@gmail.com>
2016-12-22 20:48:18 +0300

    Creates a data to database

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,35 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Client::class, function (Faker\Generator $faker) {
+
+    $username = str_random(8);
+
+    return [
+        'name'          => $faker->name,
+        'tg_chatid'     => $faker->randomNumber(),
+        'tg_username'   => $username,
+        'rating'        => $faker->randomFloat(4),
+        'comment'       => $username
+    ];
+});
+
+$factory->define(App\City::class, function (Faker\Generator $faker) {
+    return [
+        'name'  => $faker->city
+    ];
+});
+
+$factory->define(App\Goods::class, function (Faker\Generator $faker) {
+    return [
+        'name'      => $faker->name,
+    ];
+});
+
+$factory->define(App\Miner::class, function (Faker\Generator $faker) {
+    return [
+        'name'  => $faker->name,
+        'ante'  => 150
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //$this->call(ClientsTableSeeder::class);
+        //$this->call(CitysTableSeeder::class);
+
+        $this->citiesGoodsAndPriceWithMiner();
+    }
+
+    public function citiesGoodsAndPriceWithMiner()
+    {
+        $cities = factory(App\City::class, 2)->create()->each(function ($city) {
+            $city->goods()->saveMany([
+                factory(App\Goods::class)->make(),
+                factory(App\Goods::class)->make()
+            ]);
+        })->each(function ($city) {
+            $city->goods->each(function ($goods) {
+                $goods->goodsPrice()->saveMany([
+                    new App\GoodsPrice([
+                        'miner_id'  => factory(App\Miner::class)->create()->id,
+                        'weight'    => 0.5,
+                        'address'   => 'Here buyed a address',
+                        'cost'      => 1500
+                    ]),
+                    new App\GoodsPrice([
+                        'miner_id'  => factory(App\Miner::class)->create()->id,
+                        'weight'    => 1,
+                        'address'   => 'Here buyed a address',
+                        'cost'      => 2500
+                    ])
+                ]);
+            });
+        });
     }
 }
