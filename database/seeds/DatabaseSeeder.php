448ad9d4069043a5a651149bc632528f4e0902bd
Orel <xvab2byte@gmail.com>
2016-12-23 13:05:30 +0300

    Seed a clients table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(ClientsTableSeeder::class);
-        //$this->call(CitysTableSeeder::class);
+        $this->call(ClientsTableSeeder::class);
 
         $this->citiesGoodsAndPriceWithMiner();
     }
