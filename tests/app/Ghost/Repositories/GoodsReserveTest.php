9c6585a6818a7209e006d6b049cd226116ca27ed
Orel <xvab2byte@gmail.com>
2016-12-28 16:46:42 +0300

    Update test GoodsReserveTest

--- tests/app/Ghost/Repositories/GoodsReserveTest.php
+++ tests/app/Ghost/Repositories/GoodsReserveTest.php
@@ -31,57 +31,45 @@ class GoodsReserveTest extends TestCase
 
     public function test_check_expired()
     {
-        $city  = $this->createMiner();
-        $goods = $this->createGoods();
-
-        $goodsPrice = $this->goodsManager->addGoodsPrice([
-            'goods_id'  => $goods->id,
-            'city_id'   => $goods->city_id,
-            'miner_id'  => $city->id,
-            'weight'    => 0.5,
-            'address'   => 'Prospect costa 277',
-            'cost'      => 1000
-        ]);
+        $models = $this->testTools->clientWithOrder();
 
         $goodsReserve = $this->goodsReserve->makeReservation([
-            'goods_price_id'    => $goodsPrice->id,
-            'client_id'         => $this->createClient()->id,
+            'goods_price_id'    => $models->goods_price->id,
+            'client_id'         => $models->client->id,
             'time'              => Carbon::now()->addDay(1)->toDateTimeString()
         ]);
 
         $this->assertGreaterThan(0, $this->goodsReserve->checkExpired($goodsReserve->id));
-
-        $goodsReserve->delete();
-        $goodsPrice->delete();
     }
 
     public function test_cancel_reserve()
     {
-        $goodsPrice = $this->createGoodsPrice();
+        $models = $this->testTools->clientWithOrder();
 
         $goodsReserve = $this->goodsReserve->makeReservation([
-            'goods_price_id'    => $goodsPrice->id,
-            'client_id'         => $this->createClient()->id,
+            'goods_price_id'    => $models->goods_price->id,
+            'client_id'         => $models->client->id,
             'time'              => Carbon::now()->addDay(1)->toDateTimeString()
         ]);
 
         $this->goodsReserve->cancel($goodsReserve->id);
-
-        $goodsPrice->delete();
     }
 
     public function test_perform()
     {
-        $goodsPrice = $this->createGoodsPrice();
+        $models = $this->testTools->clientWithOrder();
 
         $goodsReserve = $this->goodsReserve->makeReservation([
-            'goods_price_id'    => $goodsPrice->id,
-            'client_id'         => $this->createClient()->id,
+            'goods_price_id'    => $models->goods_price->id,
+            'client_id'         => $models->client->id,
             'time'              => Carbon::now()->addDay(1)->toDateTimeString()
         ]);
 
         $this->goodsReserve->perform($goodsReserve->id);
+    }
 
-        $goodsPrice->delete();
+    public function test_terminate()
+    {
+        $this->testTools->cleaningTemporaryRows();
     }
 }
