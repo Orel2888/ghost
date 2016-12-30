051fbb5eef96755a698a47ec27d54b65f2460a22
Orel <xvab2byte@gmail.com>
2016-12-31 00:09:13 +0300

    Test to start users api

--- tests/ApiTesting/UsersApi.php
+++ tests/ApiTesting/UsersApi.php
@@ -2,7 +2,32 @@
 
 class UsersApi extends BaseApi
 {
-    public function find($username)
+    protected $apiMethodMaps = [
+        'users.find'
+    ];
+
+    /**
+     * @throws UsersApiException
+     * @param $tgchatId
+     * @return mixed
+     */
+    public function usersFind($tgchatId)
+    {
+        try {
+            $response = $this->http->request('GET', 'users.find', [
+                'query' => [
+                    'tg_chatid'     => $tgchatId,
+                    'access_token'  => $this->getToken(true)
+                ]
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+        }
+
+        return $this->handleResponseApi($response);
+    }
+
+    public function usersReg($params)
     {
 
     }
