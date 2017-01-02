18638e43499a523360c89abcef6dd9e3347e3d2c
Orel <xvab2byte@gmail.com>
2017-01-02 19:32:52 +0300

    End users api

--- tests/ApiTesting/UsersApi.php
+++ tests/ApiTesting/UsersApi.php
@@ -3,7 +3,10 @@
 class UsersApi extends BaseApi
 {
     protected $apiMethodMaps = [
-        'users.find'
+        'users.find',
+        'users.reg',
+        'users.update',
+        'users.purse'
     ];
 
     /**
@@ -27,8 +30,47 @@ class UsersApi extends BaseApi
         return $this->handleResponseApi($response);
     }
 
+    /**
+     * @throws UsersApiException
+     * @param $params
+     * @return mixed
+     */
     public function usersReg($params)
     {
+        try {
+            $response = $this->http->request('POST', 'users.reg', [
+                'form_params'   => array_merge($params, ['access_token'  => $this->getToken(true)])
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+        }
 
+        return $this->handleResponseApi($response);
+    }
+
+    public function usersUpdate($params)
+    {
+        try {
+            $response = $this->http->request('POST', 'users.update', [
+                'form_params'   => array_merge($params, ['access_token'  => $this->getToken(true)])
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+        }
+
+        return $this->handleResponseApi($response);
+    }
+
+    public function usersPurse()
+    {
+        try {
+            $response = $this->http->request('GET', 'users.purse', [
+                'query'   => ['access_token'  => $this->getToken(true)]
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+        }
+
+        return $this->handleResponseApi($response);
     }
 }
--- tests/app/Ghost/Api/UsersApiTest.php
+++ tests/app/Ghost/Api/UsersApiTest.php
@@ -9,6 +9,11 @@ class UsersApiTest extends TestCase
      */
     public $usersApi;
 
+    /**
+     * @var DataStore
+     */
+    public $dataStore;
+
     public function setUp()
     {
         parent::setUp();
@@ -27,36 +32,60 @@ class UsersApiTest extends TestCase
     {
         $client = Client::first();
 
-        $responseData = $this->usersApi->find($client->tg_chatid);
+        $this->usersApi->run('users.find', $client->tg_chatid, function ($responseData, $exception) use($client) {
+            BaseApi::throwException($exception);
 
-        $this->assertEquals($client->getAttributes(), (array) $responseData->data);
+            $this->assertEquals($client->getAttributes(), (array) $responseData->data);
+        });
     }
 
     public function test_find_not_exists_user()
     {
-        try {
-            $this->usersApi->find(222111211111);
-        } catch (UsersApiException $e) {
-            $this->assertEquals(404, $e->getStatusCode());
-        }
+        $this->usersApi->run('users.find', 222111211111, function ($responseData, $exception) {
+            $this->assertEquals(404, $exception->getStatusCode());
+        });
     }
 
     public function test_registration()
     {
-        /*try {
-            $responseData = $this->usersApi->reg();
-        } catch (UsersApiException $e) {
-
-        }*/
-
-        $this->usersApi->run('users.find', [
+        $this->usersApi->run('users.reg', [
             'name'          => 'newusertest',
             'tg_username'   => 'username',
-            'tg_chatid'     => '44678'
+            'tg_chatid'     => 44678
         ], function ($responseData, $exception) {
             BaseApi::throwException($exception);
 
-            $this->assertNotEmpty($responseData->client_id);
+            $this->assertTrue(is_numeric($responseData->client_id));
+
+            $this->testTools->storage->add('clients', $responseData->client_id);
         });
+    }
+
+    public function test_update()
+    {
+        $client = Client::first();
+
+        $this->usersApi->run('users.update', [
+            'name'          => 'new_name',
+            'tg_username'   => 'new_username',
+            'tg_chatid'     => $client->tg_chatid
+        ], function ($responseData, $e) {
+            BaseApi::throwException($e);
+
+        });
+    }
+
+    public function test_get_purse()
+    {
+        $this->usersApi->run('users.purse', null, function ($responseData, $e) {
+            BaseApi::throwException($e);
+
+            $this->assertNotEmpty($responseData->data->phone);
+        });
+    }
+
+    public function test_clear()
+    {
+        $this->testTools->cleaningTemporaryRows();
     }
 }
