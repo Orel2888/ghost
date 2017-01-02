fe18b858dbd40cf84cff2de42d5342f2114a1164
Orel <xvab2byte@gmail.com>
2017-01-02 19:33:28 +0300

    Goods api test

--- tests/ApiTesting/GoodsApi.php
+++ tests/ApiTesting/GoodsApi.php
@@ -1,7 +1,21 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: orel
- * Date: 02.01.17
- * Time: 17:11
- */
+
+class GoodsApi extends BaseApi
+{
+    protected $apiMethodMaps = [
+        'goods.pricelist'
+    ];
+
+    public function goodsPricelist()
+    {
+        try {
+            $response = $this->http->request('GET', 'goods.pricelist', [
+                'query' => ['access_token' => $this->getToken(true)]
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+        }
+
+        return $this->handleResponseApi($response);
+    }
+}
