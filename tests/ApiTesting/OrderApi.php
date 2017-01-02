3d91822a46b36c005697cb9f417130749c41bdfb
Orel <xvab2byte@gmail.com>
2017-01-02 19:33:47 +0300

    Order api start test

--- tests/ApiTesting/OrderApi.php
+++ tests/ApiTesting/OrderApi.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: orel
- * Date: 02.01.17
- * Time: 17:21
- */
+
+class OrderApi extends BaseApi
+{
+    protected $apiMethodMaps = [
+        'order.create',
+        'order.find',
+        'order.list',
+        'order.del',
+        'order.delall'
+    ];
+
+    public function orderCreate(array $params)
+    {
+        try {
+            $response = $this->http->request('POST', 'order.create', [
+                'form_params'   => array_merge($params, ['access_token' => $this->getToken(true)])
+            ]);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+            $response = $e->getResponse();
+        }
+
+        return $this->handleResponseApi($response);
+    }
+}
--- tests/app/Ghost/Api/OrderApiTest.php
+++ tests/app/Ghost/Api/OrderApiTest.php
@@ -1,6 +1,11 @@
 <?php
 
-class OrderTest extends TestCase
+use App\{
+    GoodsPrice,
+    Client
+};
+
+class OrderApiTest extends TestCase
 {
     /**
      * @var OrderApi
@@ -9,6 +14,8 @@ class OrderTest extends TestCase
 
     public function setUp()
     {
+        parent::setUp();
+
         $this->api = new OrderApi();
 
         $this->api->saveToken();
@@ -21,6 +28,68 @@ class OrderTest extends TestCase
 
     public function test_create()
     {
+        $client = Client::whereNotNull('comment')->first();
 
+        // Create one a order
+        $goodsFromPrice = GoodsPrice::first();
+
+        $this->api->run('order.create', [
+            'goods_id'  => $goodsFromPrice->goods_id,
+            'weight'    => $goodsFromPrice->weight,
+            'count'     => 1,
+            'client_id' => $client->id
+        ], function ($responseData, $e) {
+            BaseApi::throwException($e);
+
+            $this->assertNotEmpty($responseData->data);
+
+            $this->testTools->storage->add('goods_orders', $responseData->data->order_ids[0]);
+        });
+
+        // Create a few orders
+        $goodsFromPrice = $this->testTools->createGoodsToPrice(2);
+
+        $this->api->run('order.create', [
+            'goods_id'  => $goodsFromPrice->first()->goods_id,
+            'weight'    => $goodsFromPrice->first()->weight,
+            'count'     => 2,
+            'client_id' => $client->id
+        ], function ($responseData, $e) {
+            BaseApi::throwException($e);
+
+            $this->assertEquals(2, $responseData->data->count);
+
+            foreach ($responseData->data->order_ids as $orderId) {
+                $this->testTools->storage->add('goods_orders', $orderId);
+            }
+        });
+    }
+
+    public function test_create_and_purchase()
+    {
+        $client = Client::whereNotNull('comment')->first();
+
+        $goodsFromPrice = GoodsPrice::first();
+
+        $client->update(['balance' => $goodsFromPrice->cost]);
+
+        $this->api->run('order.create', [
+            'goods_id'  => $goodsFromPrice->goods_id,
+            'weight'    => $goodsFromPrice->weight,
+            'count'     => 1,
+            'client_id' => $client->id
+        ], function ($responseData, $e) {
+            BaseApi::throwException($e);
+
+            $this->assertNotEmpty($responseData->data);
+            $this->assertEquals(1, $responseData->data->order_processed);
+
+            $this->testTools->storage->add('goods_orders', $responseData->data->order_ids[0]);
+        });
+    }
+
+    public function test_clear()
+    {
+        $this->testTools->cleaningTemporaryRows();
     }
 }
