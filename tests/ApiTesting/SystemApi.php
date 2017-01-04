42626ebec885cefc6893c02c6f439fdfc3368881
Orel <xvab2byte@gmail.com>
2017-01-04 20:45:44 +0300

    Test system api

--- tests/ApiTesting/SystemApi.php
+++ tests/ApiTesting/SystemApi.php
@@ -1,7 +1,28 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: orel
- * Date: 04.01.17
- * Time: 12:24
- */
+
+class SystemApi extends BaseApi
+{
+    protected $apiMethodMaps = [
+        'sys.processing_goods_orders',
+        'sys.purse_update_balance'
+    ];
+
+    /**
+     * @param Closure $closure
+     * @return bool|mixed
+     */
+    public function sysProcessingGoodsOrders(Closure $closure)
+    {
+        return $this->run('sys.processing_goods_orders', 'GET', null, $closure);
+    }
+
+    /**
+     * @param $params
+     * @param Closure $closure
+     * @return bool|mixed
+     */
+    public function sysPurseUpdateBalance($params, Closure $closure)
+    {
+        return $this->run('sys.purse_update_balance', 'POST', $params, $closure);
+    }
+}
