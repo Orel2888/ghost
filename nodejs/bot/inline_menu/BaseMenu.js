0c0802de1f96db2801103aadbb09a610290ebf41
Orel <xvab2byte@gmail.com>
2017-01-27 20:52:08 +0300

    Added

--- nodejs/bot/inline_menu/BaseMenu.js
+++ nodejs/bot/inline_menu/BaseMenu.js
@@ -10,11 +10,68 @@ class BaseMenu {
         this.app      = app
         this.menu     = menu
         this.botScope = botScope
-        this.params   = params
+        this.params   = params || {}
+
+        this._commonButtons = {
+            start: (params) => {
+                return {
+                    text: '❰ Главная',
+                    callback: () => {
+                        return this.app.includeMenu('Start', this.botScope, params).run()
+                    }
+                }
+            }
+        }
     }
 
     run() {
         throw new Error('Not implemented')
+    }
+
+    /**
+     * No tested
+     * @param requiredParams
+     */
+    canRequiredParams(requiredParams) {
+        for (let paramName in requiredParams) {
+
+            if (!this.params.hasOwnProperty(paramName) || !(this.params[paramName] instanceof requiredParams[paramName])) {
+                let error = `Not required param ${paramName}`
+
+                this.app.logger.error({BaseMenu: error})
+
+                throw new Error(error)
+            }
+        }
+    }
+
+    /**
+     * Create layout two column and end one
+     * @param countItems
+     * @param nextLayout
+     * @param nextNums
+     * @returns {Array}
+     */
+    layoutTwoColumns(countItems, nextLayout, ...nextNums) {
+        let layout = []
+        let i = 0;
+
+        while (i < Math.floor(countItems / 2)) {
+            layout.push(2)
+            i++
+        }
+
+        if (countItems % 2 > 0) layout.push(1)
+
+        if (typeof nextLayout == 'object' && nextLayout.length) {
+            nextLayout.forEach(num => layout.push(num))
+        } else {
+            layout.push(nextLayout)
+        }
+
+        if (nextNums.length) nextNums.forEach(num => layout.push(num))
+
+        return layout
     }
 }
 
