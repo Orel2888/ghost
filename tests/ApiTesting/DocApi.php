75ced17b88632ebe1094c1eda95b45f7d5b04aeb
Orel <xvab2byte@gmail.com>
2017-02-27 17:06:31 +0300

    Generator docs by api methods

--- tests/ApiTesting/DocApi.php
+++ tests/ApiTesting/DocApi.php
@@ -1,7 +1,73 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: bb
- * Date: 27.02.17
- * Time: 15:52
- */
+
+class DocApi
+{
+    public $pathDocFile;
+
+    public function __construct($docFile = null)
+    {
+        if (is_null($docFile)) {
+            $this->pathDocFile = 'api_docs.json';
+        } else {
+            $this->pathDocFile = $docFile;
+        }
+    }
+
+    public function writeDocApi($apiMethod, $httpMethod, $formParams, $response)
+    {
+        $docs = $this->getDocApi(true);
+
+        $chunksMethod = explode('.', $apiMethod);
+
+        list($firstNameMethod, $lastNameMethod) = $chunksMethod;
+
+        if (!isset($docs[$firstNameMethod])) {
+            $docs[$firstNameMethod] = [];
+        }
+
+        if (!isset($docs[$firstNameMethod]['requests'])) {
+            $docs[$firstNameMethod]['requests'] = [];
+        }
+
+        $docRequests = $docs[$firstNameMethod]['requests'];
+
+        $duplicateRequests = array_filter($docRequests, function ($docRequest) use($apiMethod, $httpMethod, $formParams, $response) {
+            return $docRequest['method'] == $apiMethod
+                && $docRequest['http_method'] == $httpMethod
+                && $docRequest['form_params'] == $formParams
+                && $docRequest['response'] == $response;
+        });
+
+        $docUpdated = false;
+
+        if (!count($duplicateRequests)) {
+            $docRequests[] = [
+                'method'        => $apiMethod,
+                'http_method'   => $httpMethod,
+                'form_params'   => $formParams,
+                'response'      => $response
+            ];
+
+            $docs[$firstNameMethod]['requests'] = $docRequests;
+            $docUpdated = true;
+        }
+
+        if ($docUpdated) {
+            Storage::put($this->pathDocFile, json_encode($docs, JSON_PRETTY_PRINT));
+        }
+
+        return $docUpdated;
+    }
+
+    public function getDocApi($toArray = false)
+    {
+        return Storage::has($this->pathDocFile) ? json_decode(Storage::get($this->pathDocFile), $toArray) : [];
+    }
+
+    public function clearDocs()
+    {
+        Storage::put($this->pathDocFile, json_encode([]));
+
+        return $this;
+    }
+}
